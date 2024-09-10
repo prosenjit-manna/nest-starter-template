13253a3e17bf41cd2c65a23e2f1248c76af9d953
Prosenjit Manna <prosenjit@itobuz.com>
2024-09-10 21:07:23 +0530

    create user input name is optional

--- src/User/create-user.dto.ts
+++ src/User/create-user.dto.ts
@@ -1,7 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field() email: string;
-  @Field() name: string;
+  @Field(() => String, { nullable: false })
+  @IsEmail()
+  email: string;
+
+
+  @Field(() => String, { nullable: true })
+  name: string;
+
 }
