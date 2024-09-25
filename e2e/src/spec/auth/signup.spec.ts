91e4c00e879bffd7cc9437a94cc070412cae64b7
Prosenjit Manna <prosenjit@itobuz.com>
2024-09-25 20:29:32 +0530

    Keep calm and commit

---
+++ e2e/src/spec/auth/signup.spec.ts
@@ -0,0 +1,7 @@
+describe('Password Reset Functionality', () => {
+    test('should send a password reset email to the user', () => {});
+    test('should return an error if the email is not registered', () => {});
+    test('should reset the password when provided with a valid token', () => {});
+    test('should return an error if the token is invalid or expired', () => {});
+    test('should return an error if the new password does not meet criteria', () => {});
+  });
