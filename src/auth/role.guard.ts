e05c665495caea5b6d79ead848b303f27154c8e7
Prosenjit Manna <prosenjit@itobuz.com>
2024-10-25 20:15:24 +0530

    roles guard implemented for now only for roles list

---
+++ src/auth/role.guard.ts
@@ -0,0 +1,76 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prisma: PrismaService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+    // Retrieve roles metadata set by `@SetMetadata`
+    const privilegeGroup = this.reflector.get<string>('privilegeGroup', context.getHandler());
+    const privilegeName = this.reflector.get<string>('privilegeName', context.getHandler());
+
+    const ctx = GqlExecutionContext.create(context);
+
+    // Retrieve user from request object
+    const request = ctx.getContext().req as Request;
+
+    const user = request.user;
+
+
+    if (!user) {
+      return false;
+    }
+
+    // console.log('user', user);
+
+    // Retrieve roles data from database
+    const rolesData = await this.prisma.userRole.findMany({
+      where: {
+        userId: user.id
+      }
+    });
+
+    // Retrieve privileges data from database
+    const privileges = await this.prisma.rolePrivilege.findMany({
+      where: {
+        roleId: {
+          in: rolesData?.map(role => role.roleId)
+        },
+      },
+      include: {
+        privilege: true
+      }
+    });
+    
+    // console.log('rolesData', rolesData);
+    console.log('privileges', privileges);
+
+    // create a array of flat privileges
+    const privilegesFlatArr: any[] = [];
+    
+    privileges.forEach(p => {
+      privilegesFlatArr.push({
+        id: p.privilege.id,
+        name: p.privilege.name,
+        group: p.privilege.group,
+        type: p.privilege.type
+      });
+    });
+
+    console.log('privileges Arr', privilegesFlatArr);
+
+    // Check if user has the required privilege
+    const hasPrivilege = privilegesFlatArr.some(p => p.group === privilegeGroup && p.name === privilegeName);
+    console.log('hasPrivilege', hasPrivilege);
+
+   return hasPrivilege;
+  }
+}
--- src/roles/role-list/role-list.service.ts
+++ src/roles/role-list/role-list.service.ts
@@ -1,18 +1,24 @@
-import { Injectable, UseGuards } from '@nestjs/common';
+import { Injectable, SetMetadata, UseGuards } from '@nestjs/common';
 import { Args, Query } from '@nestjs/graphql';
 import { PrismaService } from 'src/prisma.service';
 import { RoleListResponse } from './role-list.response.dto';
-import { Prisma } from '@prisma/client';
+import { Prisma, PrivilegeGroup, PrivilegeName } from '@prisma/client';
 import { Order } from 'src/shared/base-list/base-list-input.dto';
 import { paginationInputTransformer } from 'src/shared/base-list/base-list-input-transform';
 import { RoleListInput } from './role-list-input.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { RoleGuard } from 'src/auth/role.guard';
 
 @Injectable()
 @UseGuards(JwtAuthGuard)
+
 export class RoleListService {
   constructor(private prisma: PrismaService) {}
 
+  @UseGuards(RoleGuard)
+  @SetMetadata('privilegeGroup', PrivilegeGroup.ROLE)
+  @SetMetadata('privilegeName', PrivilegeName.READ)
+  
   @Query(() => RoleListResponse)
   async roleList(
     @Args('roleListInput', { nullable: true })
