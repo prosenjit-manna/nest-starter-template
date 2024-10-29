fa93fe3e439efa8fcf854140c2b9181e0d2586f9
Prosenjit Manna <prosenjit@itobuz.com>
2024-10-29 16:02:39 +0530

    post role has been set for create, update, delete

--- src/post/post-update/post-update.service.ts
+++ src/post/post-update/post-update.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Injectable, UsePipes, ValidationPipe, UseGuards, SetMetadata } from '@nestjs/common';
 import { Args, Mutation } from '@nestjs/graphql';
 import { UpdatePostResponse } from './update-post-response.dto';
 import { UpdatePostInput } from './update-post.dto';
 import { PrismaService } from 'src/prisma.service';
+import { RoleGuard } from 'src/auth/role.guard';
+import { PrivilegeGroup, PrivilegeName } from '@prisma/client';
 
 @Injectable()
 export class PostUpdateService {
   constructor(private prisma: PrismaService) {}
 
   @Mutation(() => UpdatePostResponse)
+  @UseGuards(RoleGuard)
+  @SetMetadata('privilegeGroup', PrivilegeGroup.POST)
+  @SetMetadata('privilegeName', PrivilegeName.UPDATE)
   @UsePipes(new ValidationPipe())
   async updatePost(
     @Args('postId') postId: string,
