ddb3468133335db918a6cdb7139f0decb802aaad
zeniaitobuz <zenia@itobuz.com>
2024-11-08 15:48:45 +0530

    fix: error handling done

--- e2e/src/spec/auth/login-negative-testing.spec.ts
+++ e2e/src/spec/auth/login-negative-testing.spec.ts
@@ -1,103 +1,135 @@
 import { GraphQlApi } from '../../lib/graphql-api';
-import { GraphQLError } from 'graphql';
 import { faker } from '@faker-js/faker/.';
 import { appEnv } from '../../lib/app-env';
 import { PrismaClient, UserType } from '@prisma/client';
+import { LOGIN_QUERY } from '../../graphql/login-query.gql';
+import { LoginQuery, LoginQueryVariables } from '../../gql/graphql';
 
 describe('Login module negative testing', () => {
   const api = new GraphQlApi();
 
   test(`Login with only email and blank password`, async () => {
-     const dbClient = new PrismaClient();
-     const user = await dbClient.user.findFirst({
-       where: {
-         userType: UserType.ADMIN,
-       },
-     });
-     if (!user) {
-       return;
-     }
-    try {
-      await api.login({
-        email: user.email,
-        password: '',
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const dbClient = new PrismaClient();
+    const user = await dbClient.user.findFirst({
+      where: {
+        userType: UserType.ADMIN,
+      },
+    });
+    if (!user) {
+      return;
     }
+
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: user.email, password: '' },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
+    }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
 
   test(`Login with only password and blank email`, async () => {
-    try {
-      await api.login({
-        email: '',
-        password: appEnv.SEED_PASSWORD,
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: '', password: appEnv.SEED_PASSWORD },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
 
   test(`Login with blank values in all fields`, async () => {
-    try {
-      await api.login({
-        email: '',
-        password: '',
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: '', password: '' },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
 
   test(`Login with invalid email format`, async () => {
-    try {
-      await api.login({
-        email: 'abcd',
-        password: appEnv.SEED_PASSWORD,
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: 'abcd', password: appEnv.SEED_PASSWORD },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
 
   test(`Login with excessively long email and password`, async () => {
-    try {
-      await api.login({
-        email: 'abcd'.repeat(100),
-        password: 'abcd'.repeat(50),
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: 'abcd'.repeat(100), password: 'abcd'.repeat(50) },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
 
   test(`Login with wrong email and password`, async () => {
-    try {
-      await api.login({
-        email: faker.internet.email(),
-        password: faker.lorem.word(),
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: {
+          email: faker.internet.email(),
+          password: faker.lorem.word(),
+        },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
-  
+
   test(`Login with space in email and password field`, async () => {
-    try {
-      await api.login({
-        email: '   ',
-        password: '  ',
-      });
-    } catch (error) {
-      if (error instanceof GraphQLError)
-        expect(error.message).toBe('Invalid email or password');
+    const loginResponse = await api.graphql.query<
+      LoginQuery,
+      LoginQueryVariables
+    >({
+      query: LOGIN_QUERY,
+      variables: {
+        loginInput: { email: '   ', password: '   ' },
+      },
+    });
+    if (!loginResponse.errors) {
+      throw new Error('Expected an error, but none was returned');
     }
+    expect(loginResponse.errors[0].message).toBe('Invalid email or password');
   });
-  
 });
