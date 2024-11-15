51141af5217ed1ec3d50dfdc14fc00d4c026ede6
Prosenjit Manna <prosenjit@itobuz.com>
2024-11-15 17:21:01 +0530

    feat: password change input validator updated

--- src/auth/password-reset/password-reset-input.dto.ts
+++ src/auth/password-reset/password-reset-input.dto.ts
@@ -1,8 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Matches, MinLength } from 'class-validator';
 
 @InputType()
 export class PasswordResetInput {
   @Field(() => String)
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/, {
+    message:
+      'Password must contain at least one letter, one number, and one special character',
+  })
   password: string;
 
 }
