4161083e366dc9a265539e6b2ffc0858d08bfa72
zeniaitobuz <zenia@itobuz.com>
2024-11-18 17:54:58 +0530

    merged

--- e2e/src/gql/gql.ts
+++ e2e/src/gql/gql.ts
@@ -32,6 +32,7 @@ const documents = {
     "\n  query Role {\n    listBasePrivilege {\n      privilege {\n        name\n        group\n        id\n        type\n        createdAt\n        updatedAt\n        deletedAt\n      }\n    }\n  }\n": types.RoleDocument,
     "\n  mutation RefreshAccessToken($refreshAccessTokenInput: RefreshAccessTokenInput!) {\n  refreshAccessToken(refreshAccessTokenInput: $refreshAccessTokenInput) {\n    token\n    refreshToken\n  }\n}\n": types.RefreshAccessTokenDocument,
     "\n  mutation RequestPasswordReset($passwordReset: PasswordResetRequestInput!) {\n    requestPasswordReset(passwordReset: $passwordReset) {\n      message\n    }\n  }\n": types.RequestPasswordResetDocument,
+    "\n mutation SendInvitation($sendInvitationInput: SendInvitationInput!) {\n  sendInvitation(sendInvitationInput: $sendInvitationInput) {\n    success\n  }\n}\n": types.SendInvitationDocument,
     "\n  mutation Signup($signupInput: SignupInput!) {\n    signup(signupInput: $signupInput) {\n      id\n    }\n  }\n": types.SignupDocument,
     "\n  mutation UnAssignRole($unAssignRoleInput: UnAssignRoleInput!) {\n    unAssignRole(unAssignRoleInput: $unAssignRoleInput) {\n      success\n    }\n  }\n": types.UnAssignRoleDocument,
     "\n  mutation UpdatePost($postId: String!, $updatePostInput: UpdatePostInput!) {\n    updatePost(postId: $postId, updatePostInput: $updatePostInput) {\n      id\n    }\n  }\n": types.UpdatePostDocument,
@@ -130,6 +131,10 @@ export function graphql(source: "\n  mutation RefreshAccessToken($refreshAccessT
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  mutation RequestPasswordReset($passwordReset: PasswordResetRequestInput!) {\n    requestPasswordReset(passwordReset: $passwordReset) {\n      message\n    }\n  }\n"): (typeof documents)["\n  mutation RequestPasswordReset($passwordReset: PasswordResetRequestInput!) {\n    requestPasswordReset(passwordReset: $passwordReset) {\n      message\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n mutation SendInvitation($sendInvitationInput: SendInvitationInput!) {\n  sendInvitation(sendInvitationInput: $sendInvitationInput) {\n    success\n  }\n}\n"): (typeof documents)["\n mutation SendInvitation($sendInvitationInput: SendInvitationInput!) {\n  sendInvitation(sendInvitationInput: $sendInvitationInput) {\n    success\n  }\n}\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
---
+++ e2e/src/graphql/send-invitation-mutation.gql.ts
@@ -0,0 +1,9 @@
+import { graphql } from 'gql';
+
+export const SEND_INVITATION_MUTATION = graphql(`
+ mutation SendInvitation($sendInvitationInput: SendInvitationInput!) {
+  sendInvitation(sendInvitationInput: $sendInvitationInput) {
+    success
+  }
+}
+`);
