5128e9ce335a142dbf8f14cd42085b44237ecd0f
zeniaitobuz <zenia@itobuz.com>
2024-11-19 17:36:36 +0530

    fix: membership negative test added

--- e2e/src/spec/workspace/membership/membership-negative-testing.spec.ts
+++ e2e/src/spec/workspace/membership/membership-negative-testing.spec.ts
@@ -32,167 +32,162 @@ describe('Membership invitation module', () => {
     await prisma.$disconnect();
   });
 
-  [UserType.ADMIN, UserType.SUPER_ADMIN].forEach((type) => {
-    test(`Login as a ${type}`, async () => {
-      user = await dbClient.user.findFirst({
-        where: {
-          userType: UserType.ADMIN,
-          isVerified: true,
-        },
-      });
-
-      if (!user) {
-        return;
-      }
-
-      const response = await api.login({
-        email: user.email,
-        password: appEnv.SEED_PASSWORD,
-      });
-
-      expect(response.data).toBeDefined();
+  test(`Login as a Admin`, async () => {
+    user = await dbClient.user.findFirst({
+      where: {
+        userType: UserType.ADMIN,
+        isVerified: true,
+      },
     });
 
-    test(`Get user list and fetch a random user id ${type}`, async () => {
-      const userList = await api.graphql.query<
-        GetUsersQuery,
-        GetUsersQueryVariables
+    if (!user) {
+      return;
+    }
+
+    const response = await api.login({
+      email: user.email,
+      password: appEnv.SEED_PASSWORD,
+    });
+
+    expect(response.data).toBeDefined();
+  });
+
+  test(`Get user list and fetch a random user id Admin`, async () => {
+    const userList = await api.graphql.query<
+      GetUsersQuery,
+      GetUsersQueryVariables
+    >({
+      query: USER_LIST,
+    });
+    userId = sample(userList.data.getUsers)?.id;
+    userEmail = sample(userList.data.getUsers)?.email;
+    expect(userList.data.getUsers.length).not.toBe(0);
+  });
+
+  test(`Send invitation with a user Id who is already in the workspace Admin`, async () => {
+    const workspace = await dbClient.workspaceMembership.findFirst({
+      where: {
+        userId: userId,
+      },
+    });
+    workspaceID = workspace?.workspaceId;
+    if (workspaceID && userId) {
+      const sendInvitation = await api.graphql.mutate<
+        SendInvitationMutation,
+        SendInvitationMutationVariables
       >({
-        query: USER_LIST,
-      });
-      userId = sample(userList.data.getUsers)?.id;
-      userEmail = sample(userList.data.getUsers)?.email;
-      expect(userList.data.getUsers.length).not.toBe(0);
-    });
-
-    test(`Send invitation with a user Id who is already in the workspace ${type}`, async () => {
-      const workspace = await dbClient.workspaceMembership.findFirst({
-        where: {
-          userId: userId,
-        },
-      });
-      workspaceID = workspace?.workspaceId;
-      if (workspaceID && userId) {
-        const sendInvitation = await api.graphql.mutate<
-          SendInvitationMutation,
-          SendInvitationMutationVariables
-        >({
-          mutation: SEND_INVITATION_MUTATION,
-          variables: {
-            sendInvitationInput: {
-              userId: userId,
-              workspaceId: workspaceID,
-            },
-          },
-        });
-        if (!sendInvitation.errors) {
-          throw new Error('Expected an error, but none was returned');
-        }
-        console.log(sendInvitation.errors[0]);
-        // expect(sendInvitation.errors[0].message).toContain(
-        //   'Foreign key constraint failed on the field: `WorkspaceMembership_userId_fkey (index)',
-        // );
-      }
-    });
-
-    test(`Send invitation with a wrong workspace Id ${type}`, async () => {
-      const workspace = await dbClient.workspaceMembership.findFirst({
-        where: {
-          userId: { not: user?.id },
-        },
-      });
-      if (userId && workspace) {
-        const sendInvitation = await api.graphql.mutate<
-          SendInvitationMutation,
-          SendInvitationMutationVariables
-        >({
-          mutation: SEND_INVITATION_MUTATION,
-          variables: {
-            sendInvitationInput: {
-              userId: userId,
-              workspaceId: workspace?.workspaceId,
-            },
-          },
-        });
-        if (!sendInvitation.errors) {
-          throw new Error('Expected an error, but none was returned');
-        }
-        console.log(sendInvitation.errors[0]);
-        // expect(sendInvitation.errors[0].message).toContain(
-        //   'Foreign key constraint failed on the field: `WorkspaceMembership_workspaceId_fkey (index)',
-        // );
-      }
-    });
-
-    test(`Verify invitation ${type}`, async () => {
-      const verifyInvitation = await api.graphql.mutate<
-        AcceptInvitationMutation,
-        AcceptInvitationMutationVariables
-      >({
-        mutation: VERIFY_INVITATION_MUTATION,
+        mutation: SEND_INVITATION_MUTATION,
         variables: {
-          acceptInvitationInput: {
-            token: onboardingToken as string,
+          sendInvitationInput: {
+            userId: userId,
+            workspaceId: workspaceID,
           },
         },
       });
-      if (!verifyInvitation.errors) {
+      if (!sendInvitation.errors) {
         throw new Error('Expected an error, but none was returned');
       }
-      expect(verifyInvitation.errors[0].message).toContain(
-        'Invalid invitation token!',
+      expect(sendInvitation.errors[0].message).toContain(
+        'User already a member of this workspace',
       );
-    });
+    }
+  });
 
-    test(`Send invitation to a user `, async () => {
-      const workspace = await dbClient.workspaceMembership.findFirst({
-        where: {
-          userId: user?.id,
+  test(`Send invitation with a workspace which is not available for the logged in user`, async () => {
+    const workspace = await dbClient.workspaceMembership.findFirst({
+      where: {
+        userId: { not: user?.id },
+      },
+    });
+    if (userId && workspace) {
+      const sendInvitation = await api.graphql.mutate<
+        SendInvitationMutation,
+        SendInvitationMutationVariables
+      >({
+        mutation: SEND_INVITATION_MUTATION,
+        variables: {
+          sendInvitationInput: {
+            userId: userId,
+            workspaceId: workspace?.workspaceId,
+          },
         },
       });
-      workspaceID = workspace?.workspaceId;
-      if (workspaceID && userId) {
-        const sendInvitation = await api.graphql.mutate<
-          SendInvitationMutation,
-          SendInvitationMutationVariables
-        >({
-          mutation: SEND_INVITATION_MUTATION,
-          variables: {
-            sendInvitationInput: {
-              userId: userId,
-              workspaceId: workspaceID,
-            },
+      if (!sendInvitation.errors) {
+        throw new Error('Expected an error, but none was returned');
+      }
+      expect(sendInvitation.errors[0].message).toContain('Forbidden resource');
+    }
+  });
+
+  test(`Verify invitation Admin`, async () => {
+    const verifyInvitation = await api.graphql.mutate<
+      AcceptInvitationMutation,
+      AcceptInvitationMutationVariables
+    >({
+      mutation: VERIFY_INVITATION_MUTATION,
+      variables: {
+        acceptInvitationInput: {
+          token: onboardingToken as string,
+          accept: true,
+        },
+      },
+    });
+    if (!verifyInvitation.errors) {
+      throw new Error('Expected an error, but none was returned');
+    }
+    expect(verifyInvitation.errors[0].message).toContain(
+      'Invalid invitation token!',
+    );
+  });
+
+  test(`Send invitation to a user `, async () => {
+    const workspace = await dbClient.workspaceMembership.findFirst({
+      where: {
+        userId: user?.id,
+      },
+    });
+    workspaceID = workspace?.workspaceId;
+    if (workspaceID && userId) {
+      const sendInvitation = await api.graphql.mutate<
+        SendInvitationMutation,
+        SendInvitationMutationVariables
+      >({
+        mutation: SEND_INVITATION_MUTATION,
+        variables: {
+          sendInvitationInput: {
+            userId: userId,
+            workspaceId: workspaceID,
           },
-        });
-        expect(sendInvitation.data?.sendInvitation.success).toBe(true);
-      }
-    });
+        },
+      });
+      expect(sendInvitation.data?.sendInvitation.success).toBe(true);
+    }
+  });
 
-    test(`Login as the user who has been invited`, async () => {
-      if (userEmail) {
-        const response = await api.login({
-          email: userEmail,
-          password: appEnv.SEED_PASSWORD,
-        });
-        expect(response.data).toBeDefined();
-      }
-    });
+  test(`Login as the user who has been invited`, async () => {
+    if (userEmail) {
+      const response = await api.login({
+        email: userEmail,
+        password: appEnv.SEED_PASSWORD,
+      });
+      expect(response.data).toBeDefined();
+    }
+  });
 
-    test('Check whether the user can access the workspace', async () => {
-      if (workspaceID) {
-        const listWorkspace = await api.graphql.query<
-          ListWorkSpaceQuery,
-          ListWorkSpaceQueryVariables
-        >({
-          query: LIST_WORKSPACE_QUERY,
-        });
+  test('Check whether the user can access the workspace', async () => {
+    if (workspaceID) {
+      const listWorkspace = await api.graphql.query<
+        ListWorkSpaceQuery,
+        ListWorkSpaceQueryVariables
+      >({
+        query: LIST_WORKSPACE_QUERY,
+      });
 
-        const addedWorkspace = listWorkspace.data.listWorkSpace.workspace.find(
-          (workspace) => workspace.id === workspaceID,
-        );
+      const addedWorkspace = listWorkspace.data.listWorkSpace.workspace.find(
+        (workspace) => workspace.id === workspaceID,
+      );
 
-        expect(addedWorkspace).toBe(undefined);
-      }
-    });
+      expect(addedWorkspace).toBe(undefined);
+    }
   });
 });
