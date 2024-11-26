65144916f13a74cc5038a1789675bb5f8668226e
Prosenjit Manna <prosenjit@itobuz.com>
2024-11-26 18:41:01 +0530

    fix: password strength validation NST-91

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,5 @@
 export const appConfig = {
     pageSize: 10,
     current_workspace_id: 'current_workspace_id',
+    userPasswordValidationRegex: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\S]+$/,
 }
