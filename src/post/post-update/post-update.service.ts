b922bdb51af00b4cd566aaa7b70baaf09d2a4d03
Prosenjit Manna <prosenjit@itobuz.com>
2024-11-26 18:02:57 +0530

    fix: post update workspace membership restriction updated

--- src/post/post-update/post-update.service.ts
+++ src/post/post-update/post-update.service.ts
@@ -9,6 +9,8 @@ import { PrismaService } from 'src/prisma.service';
 import { RoleGuard } from 'src/auth/role.guard';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { PostMemberShipValidation } from '../post-membership-validation';
+import { WorkspaceMemberShipGuard } from 'src/auth/workspace-membership.guard';
+import { MemberShipValidationType } from 'src/auth/membership-validation-type.enum';
 
 @UseGuards(JwtAuthGuard)
 @Resolver()
@@ -22,11 +24,17 @@ export class PostUpdateService {
   @UseGuards(RoleGuard)
   @SetMetadata('privilegeGroup', PrivilegeGroup.POST)
   @SetMetadata('privilegeName', PrivilegeName.UPDATE)
+  
+  @UseGuards(WorkspaceMemberShipGuard)
+  @SetMetadata('memberShipValidationType', MemberShipValidationType.MEMBERSHIP_VALIDITY)
+  
   async updatePost(
     @Args('postId') postId: string,
     @Args('updatePostInput') updatePostInput: UpdatePostInput,
     @Context('req') req: Request,
   ) {
+
+    this.postMemberShipValidation.validateAuthorMembership(req.memberships, (updatePostInput?.authorId || req?.user?.id) || '');
 
     const existingPost = await this.prisma.post.findUnique({
       where: { id: postId },
