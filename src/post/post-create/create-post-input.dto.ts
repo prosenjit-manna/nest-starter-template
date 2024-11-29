bf2f920ca3e559ba0af460521fd52bb27173db0a
Prosenjit Manna <prosenjit@itobuz.com>
2024-11-29 12:36:47 +0530

    cleanup

--- src/post/post-create/create-post-input.dto.ts
+++ src/post/post-create/create-post-input.dto.ts
@@ -1,14 +1,6 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, MaxLength } from 'class-validator';
-import * as sanitizeHtml from 'sanitize-html';
-
-const html = "<div>hello><script>alert('test');</script></div>";
-console.log(sanitizeHtml(html));
-// console.log(sanitizeHtml("<img src=x onerror=alert('img') />"));
-// console.log(sanitizeHtml("console.log('hello world')"));
-// console.log(sanitizeHtml("<script>alert('hello world')</script>"));
-
 @InputType()
 export class CreatePostInput {
   @Field(() => String, { nullable: false })
