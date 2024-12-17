df69bbc4e5233564d4285dd3155b963121c379fc
Prosenjit Manna <prosenjit@itobuz.com>
2024-12-17 19:03:22 +0530

    delete folder without guards

--- src/media/folder/delete-folder/delete-folder.service.ts
+++ src/media/folder/delete-folder/delete-folder.service.ts
@@ -1,63 +1,53 @@
-import { HttpStatus, SetMetadata, UseGuards } from "@nestjs/common";
-import { Args, Context, Mutation, Resolver } from "@nestjs/graphql";
-import { PrivilegeGroup, PrivilegeName } from "@prisma/client";
-import { MemberShipValidationType } from "src/auth/membership-validation-type.enum";
-import { RoleGuard } from "src/auth/role.guard";
-import { WorkspaceMemberShipGuard } from "src/auth/workspace-membership.guard";
-import { DeleteFolderInput } from "./delete-folder.input.dto";
-import { PrismaService } from "src/prisma/prisma.service";
-import { CreateAppError } from "src/shared/create-error/create-error";
+import { HttpStatus, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { DeleteFolderInput } from './delete-folder.input.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAppError } from 'src/shared/create-error/create-error';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Resolver()
 export class DeleteFolderService {
+  constructor(private prismaService: PrismaService) {}
 
-    constructor(
-      private prismaService: PrismaService,
-    ) {}
-
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Boolean)
-    @UseGuards(RoleGuard)
-    @SetMetadata('privilegeGroup', PrivilegeGroup.MEDIA)
-    @SetMetadata('privilegeName', PrivilegeName.DELETE)
-  
-    @UseGuards(WorkspaceMemberShipGuard)
-    @SetMetadata('memberShipValidationType', MemberShipValidationType.MEMBERSHIP_VALIDITY)
-  
-    async deleteFolder(
-      @Context('req') req: Request,
-      @Args('folderDeleteInput', { nullable: true })
-      folderDeleteInput: DeleteFolderInput,
-    ): Promise<boolean> {
-  
-      const post = await this.prismaService.folder.findUnique({
-        where: { id: folderDeleteInput.id, deletedAt: folderDeleteInput.fromStash ? { not: null } : null },
+  async deleteFolder(
+    @Args('folderDeleteInput', { nullable: true })
+    folderDeleteInput: DeleteFolderInput,
+  ): Promise<boolean> {
+    const post = await this.prismaService.folder.findUnique({
+      where: {
+        id: folderDeleteInput.id,
+        deletedAt: folderDeleteInput.fromStash ? { not: null } : null,
+      },
+    });
+
+    if (!post) {
+      throw new CreateAppError({
+        message: 'Folder not found',
+        httpStatus: HttpStatus.NOT_FOUND,
       });
-  
-  
-      if (!post) {
-        throw new CreateAppError({
-          message: 'Folder not found',
-          httpStatus: HttpStatus.NOT_FOUND,
-        });
-      }
-  
-      try {
-        if (folderDeleteInput.fromStash) {
-          await this.prismaService.folder.delete({ where: { id: folderDeleteInput.id } });
-        } else {
-          await this.prismaService.folder.update({
-            where: { id: folderDeleteInput.id, deletedAt: null },
-            data: { deletedAt: new Date() },
-          });
-        }
-  
-        return true;
-      } catch (error) {
-        throw new CreateAppError({
-          message: 'Unable to Delete Folder',
-          httpStatus: HttpStatus.NOT_FOUND,
-          error,
-        });
-      }
     }
+
+    try {
+      if (folderDeleteInput.fromStash) {
+        await this.prismaService.folder.delete({
+          where: { id: folderDeleteInput.id },
+        });
+      } else {
+        await this.prismaService.folder.update({
+          where: { id: folderDeleteInput.id, deletedAt: null },
+          data: { deletedAt: new Date() },
+        });
+      }
+
+      return true;
+    } catch (error) {
+      throw new CreateAppError({
+        message: 'Unable to Delete Folder',
+        httpStatus: HttpStatus.NOT_FOUND,
+        error,
+      });
+    }
+  }
 }
