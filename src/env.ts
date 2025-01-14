e67baff1066a59e411eb783a93a9c3e1d8cda26e
Prosenjit Manna <prosenjit@itobuz.com>
2025-01-14 20:33:59 +0530

    readme updated

--- src/env.ts
+++ src/env.ts
@@ -34,8 +34,8 @@ export const appEnv = cleanEnv(process.env, {
   SMTP_SENDER: str({ desc: 'Sender Email', default: 'nest-starter@example.com' }),
 
   //  Mail sending
-  MAIL_FROM_USER: str({ desc: 'Mail from user' }),
-  MAIL_FROM_EMAIL: str({ desc: 'Mail from email' }),
+  MAIL_FROM_USER: str({ default: 'example', desc: 'Mail from user' }),
+  MAIL_FROM_EMAIL: str({ default: 'example@example.com', desc: 'Mail from email' }),
 
   // FRONTEND_URL
   SIGNUP_VERIFY_URL: str({
@@ -52,7 +52,7 @@ export const appEnv = cleanEnv(process.env, {
   }),
 
   // Error Logging
-  SENTRY_URL: str({ desc: 'Please provide SENTRY URL' }),
+  SENTRY_URL: str({ default: '', desc: 'Please provide SENTRY URL' }),
 
   PRISMA_DEBUG: bool({ default: false }),
 
@@ -64,15 +64,5 @@ export const appEnv = cleanEnv(process.env, {
   // Pagination
   PAGE_SIZE: num({ default: 10 }),
 });
-
-// Read an environment variable, which is validated and cleaned during
-// and/or filtering that you specified with cleanEnv().
-appEnv.ADMIN_EMAIL; // -> 'admin@example.com'
-
-// Envalid checks for NODE_ENV automatically, and provides the following
-// shortcut (boolean) properties for checking its value:
-appEnv.isProduction; // true if NODE_ENV === 'production'
-appEnv.isTest; // true if NODE_ENV === 'test'
-appEnv.isDev; // true if NODE_ENV === 'development'
 
 export default appEnv;
--- src/shared/sentry/sentry-init.ts
+++ src/shared/sentry/sentry-init.ts
@@ -2,14 +2,16 @@ import * as Sentry from "@sentry/nestjs"
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import appEnv from "src/env";
 
-Sentry.init({
-  dsn: appEnv.SENTRY_URL,
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 0, //  Capture 100% of the transactions
-
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 0
-});
+if (appEnv.SENTRY_URL) {
+  Sentry.init({
+    dsn: appEnv.SENTRY_URL,
+    integrations: [
+      nodeProfilingIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 0, //  Capture 100% of the transactions
+  
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 0
+  });
+}
